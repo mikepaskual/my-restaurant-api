8c66b30c531a27024e8a1056ccd3ce1e99a77156
Miguel A. Pascual <mianpascual@gmail.com>
2025-06-30 19:19:33 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/mikepaskual/my_restaurant_api/MyRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.mikepaskual.my_restaurant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestaurantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikepaskual/my_restaurant_api/MyRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikepaskual.my_restaurant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
